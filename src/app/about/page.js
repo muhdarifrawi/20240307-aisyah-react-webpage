847316a1ec5ae8cccecbd932b354dcf9fdf2ba9c
muhdarifrawi <muhdarifrawi@gmail.com>
2024-03-18 21:52:29 +0800

    restructured pages and links.

---
+++ src/app/about/page.js
@@ -0,0 +1,16 @@
+import commonStyles from "../css/common.module.css"
+
+export default function About(){
+    return(
+        <section className={commonStyles.container}>
+            <h1 className={commonStyles.title}>
+                About
+            </h1>
+            <section className={commonStyles.section}>
+                <p>
+                    Nur Aisyah Mariah Binte Jailani is a 
+                </p>
+            </section>
+        </section>
+    );
+}
--- src/app/projects.js
+++
@@ -1,6 +0,0 @@
-
-export default function Projects(){
-    return(
-        <h1>This is the projects page</h1>
-    );
-}

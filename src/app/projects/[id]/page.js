7f67a3fb4f4d24a81ac3223d8109d431057be9b6
muhdarifrawi <muhdarifrawi@gmail.com>
2024-04-02 06:01:11 +0800

    created back button for project page.

--- src/app/projects/[id]/page.js
+++ src/app/projects/[id]/page.js
@@ -1,4 +1,6 @@
 import {promises as fs} from 'fs';
+import Link from 'next/link';
+import commonStyles from "../../css/common.module.css";
 
 export default async function ProjectsPage({params}){
     const file = await fs.readFile(process.cwd() + "/src/app/data/projects.json", 
@@ -11,7 +13,22 @@ export default async function ProjectsPage({params}){
         }
     });
     return(
-        <><h1>{params.id}</h1><p>{data["projects"][0]["projectYear"]}</p></>
-        
+        <section className={commonStyles.container}>
+            <section>
+                <Link href="/">
+                    <button className={commonStyles.backButton}>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" 
+                        viewBox="-150 -960 960 960" width="24">
+                        <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z"/>
+                        </svg>
+                    </button>
+                </Link>
+            </section>
+            <section>
+            <h1>{params.id}</h1>
+            <p>{data["projects"][0]["projectYear"]}</p>
+            </section>
+            
+        </section>
     );
 }

dad4bca4fa1ae2bf290eb636e6f2e451c0c9012c
muhdarifrawi <muhdarifrawi@gmail.com>
2024-04-29 18:31:46 +0800

    Edited homepage: - Mobile -> added padding to card title - Desktop -> added hovering transition

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@
 import Link from "next/link";
 import {promises as fs} from 'fs';
 import commonStyles from "./css/common.module.css"
+import Image from "next/image";
 
 export default async function Home() {
   // creating and iterating the cards so it looks neater in code. 
@@ -12,9 +13,8 @@ export default async function Home() {
                                     "utf8");
   const data  = JSON.parse(file);
   const projectsMobile = Object.keys(data.projects).map((obj,i) => {
-    const images = data.projects[obj].projectPictures.map(img => {
-      return <img className={commonStyles.image} src={img}/>
-    })
+    const imageLinks = data.projects[obj].projectPictures[0]
+    const images = <Image className={commonStyles.image} src={imageLinks} width={100} height={100}/>
     const id = data.projects[obj].projectId;
     console.log("id from pages", id);
     return (
@@ -30,12 +30,10 @@ export default async function Home() {
   });
   const projectsDesktop = Object.keys(data.projects).map((obj,i) => {
     const imageLinks = data.projects[obj].projectPictures[0]
-    const images = <img className={commonStyles.image} src={imageLinks}/>
-    const dataLength = Object.keys(data.projects).length;
+    const images = <Image className={commonStyles.image} src={imageLinks} width={100} height={100} unoptimized={true}/>
     const id = data.projects[obj].projectId;
     console.log("==========");
     console.log("id from pages", id);
-    console.log(dataLength);
     return (
         <Link href={`/projects/${id}`} className={commonStyles.desktopScale} id={"desktop-" + id}>
         <section className={commonStyles.card}>
@@ -48,7 +46,7 @@ export default async function Home() {
   // This is the primary return that renders out the page
   return (
     <main className={commonStyles.container}>
-      <img src="./images/logo.png" className={commonStyles.bannerLogo}/>
+      <Image src="/images/logo.png" className={commonStyles.bannerLogo} width={360} height={100} unoptimized={true}/>
 
       <div className={commonStyles.mobile}>
         <section className={commonStyles.alignCenter}>

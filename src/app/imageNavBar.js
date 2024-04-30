44ec08f06495c2c3da8000b15e42b5b13e9207ba
muhdarifrawi <muhdarifrawi@gmail.com>
2024-04-30 11:32:01 +0800

    added styling to image nav

--- src/app/imageNavBar.js
+++ src/app/imageNavBar.js
@@ -28,7 +28,10 @@ export default function imageNavbar(data) {
                 <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
             </svg>
         </button>
-        <p id="imgNumber">{count + 1} of {totalNumberOfImages}</p>
+        <p id="imgNumber">
+            <span className={commonStyles.navImageNumbers}>{count + 1}</span> of {' '}
+            <span className={commonStyles.navImageNumbers}>{totalNumberOfImages}</span>
+        </p>
         <button className={commonStyles.imageNavBtn} onClick={handleIncrease}>
             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
                 <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
@@ -38,13 +41,9 @@ export default function imageNavbar(data) {
     }
 
     let imgPath = `../.` + data["data"][0]["projectPictures"][count];
-    // if (data["data"][0]["projectPictures"][0]) {
-    //     imgPath = `../.` + data["data"][0]["projectPictures"][0]
-    // }
+
     return (<>
         <div className={commonStyles.imageContainer}>
-            {/* <img className={commonStyles.projectImages}
-                src={imgPath} /> */}
             <Image src={imgPath} className={commonStyles.projectImages} width={100} height={100} unoptimized={true}/>
         </div>
         {navBtn}

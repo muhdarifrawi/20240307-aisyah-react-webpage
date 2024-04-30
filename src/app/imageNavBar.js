1974af5f1cbda85412f59438248ee1aa57f742db
muhdarifrawi <muhdarifrawi@gmail.com>
2024-04-30 11:25:14 +0800

    refined project images navigation.

--- src/app/imageNavBar.js
+++ src/app/imageNavBar.js
@@ -1,11 +1,13 @@
 'use client';
 import { useState } from "react";
 import commonStyles from "./css/common.module.css";
+import Image from "next/image";
 
 export default function imageNavbar(data) {
     console.log("imageNavber data: ", data["data"][0]);
     const [count, setCount] = useState(0);
     const totalNumberOfImages = data["data"][0]["projectPictures"].length;
+
     function handleIncrease() {
         if(count < totalNumberOfImages - 1){
             setCount(count + 1);
@@ -18,28 +20,34 @@ export default function imageNavbar(data) {
         }
     }
 
+    let navBtn;
+    if(totalNumberOfImages !== 1){
+        navBtn = <div className={commonStyles.imageNav}>
+        <button className={commonStyles.imageNavBtn} onClick={handleDecrease}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+                <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
+            </svg>
+        </button>
+        <p id="imgNumber">{count + 1} of {totalNumberOfImages}</p>
+        <button className={commonStyles.imageNavBtn} onClick={handleIncrease}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+                <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
+            </svg>
+        </button>
+    </div>
+    }
+
     let imgPath = `../.` + data["data"][0]["projectPictures"][count];
     // if (data["data"][0]["projectPictures"][0]) {
     //     imgPath = `../.` + data["data"][0]["projectPictures"][0]
     // }
     return (<>
         <div className={commonStyles.imageContainer}>
-            <img className={commonStyles.projectImages}
-                src={imgPath} />
+            {/* <img className={commonStyles.projectImages}
+                src={imgPath} /> */}
+            <Image src={imgPath} className={commonStyles.projectImages} width={100} height={100} unoptimized={true}/>
         </div>
-        <div className={commonStyles.imageNav}>
-            <button className={commonStyles.imageNavBtn} onClick={handleDecrease}>
-                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-                    <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
-                </svg>
-            </button>
-            <p id="imgNumber">{count + 1} of {totalNumberOfImages}</p>
-            <button className={commonStyles.imageNavBtn} onClick={handleIncrease}>
-                <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
-                    <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
-                </svg>
-            </button>
-        </div>
+        {navBtn}
     </>
     )
 }

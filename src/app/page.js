b4b6994b8cf28d04c94b6473ba44e5a12f735d14
Arif Rawi <54611941+muhdarifrawi@users.noreply.github.com>
2025-11-22 15:25:49 +0800

    Create page not available

--- src/app/page.js
+++ src/app/page.js
@@ -6,63 +6,23 @@ import Link from "next/link";
 import {promises as fs} from 'fs';
 import commonStyles from "./css/common.module.css"
 import Image from "next/image";
-import projectsJSON from "../../public/data/projects.json";
 
 export default async function Home() {
   // creating and iterating the cards so it looks neater in code. 
-  // const file = await fs.readFile("./public/data/projects.json", 
-  //                                   "utf8");
-  // const data  = JSON.parse(file);
-  const data = projectsJSON;
-  const projectsMobile = Object.keys(data.projects).map((obj,i) => {
-    const imageLinks = data.projects[obj].projectPictures[0]
-    const images = <Image className={commonStyles.image} src={imageLinks} width={100} height={100} 
-                      unoptimized={true} placeholder="blur" blurDataURL="data:images/blurimg.png"/>
-    const id = data.projects[obj].projectId;
-    console.log("id from pages", id);
-    return (
-        <Link href={`/projects/${id}`} key={id}>
-        <section className={commonStyles.card}>
-          <h1 className={commonStyles.cardTitle}>
-            {data.projects[obj].projectName}
-          </h1>
-          {images}
-        </section>
-        </Link>
-    )
-  });
-  const projectsDesktop = Object.keys(data.projects).map((obj,i) => {
-    const imageLinks = data.projects[obj].projectPictures[0]
-    const images = <Image className={commonStyles.image} src={imageLinks} width={100} height={100} 
-                    unoptimized={true} placeholder="blur" blurDataURL="data:images/blurimg.png"/>
-    const id = data.projects[obj].projectId;
-    console.log("==========");
-    console.log("id from pages", id);
-    return (
-        <Link href={`/projects/${id}`} className={commonStyles.desktopScale} id={"desktop-" + id} key={id}>
-        <section className={commonStyles.card}>
-          {images}
-        </section>
-        </Link>
-    )
-  });  
+  
 
   // This is the primary return that renders out the page
   return (
     <main className={commonStyles.container}>
-      <Link href="/about">
-        <Image src="/images/logo.png" className={commonStyles.bannerLogo} width={360} height={100}
-                   unoptimized={true} placeholder="blur" blurDataURL="data:images/blurimg.png"/>
-      </Link>
 
       <div className={commonStyles.mobile}>
         <section className={commonStyles.alignCenter}>
-          {projectsMobile}
+          <h1>This page is currently not available.</h1>
         </section>
       </div>
       <div className={commonStyles.desktop}>
         <section>
-          {projectsDesktop}
+          <h1>This page is currently not available.</h1>
         </section>
       </div>
       
